2b8d373bddf427bcd95e2595cb64740ebd1d0d30
Alan Coopersmith <alan.coopersmith@oracle.com>
2011-11-24 12:59:56 -0800

    XcursorFileSaveImages: plug memory leak on invalid input
    Error: Memory leak (CWE 401)
       Memory leak of pointer 'comments' allocated with XcursorCommentsCreate(0)
            at line 982 of src/file.c in function 'XcursorFileSaveImages'.
              'comments' allocated at line 978 with XcursorCommentsCreate(0).
              comments leaks when comments != 0 at line 981.
    
    [ This bug was found by the Parfait 0.3.7 bug checking tool.
      For more information see http://labs.oracle.com/projects/parfait/ ]
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>

--- src/file.c
+++ src/file.c
@@ -975,10 +975,13 @@ XcursorFileLoad (FILE		    *file,
 XcursorBool
 XcursorFileSaveImages (FILE *file, const XcursorImages *images)
 {
-    XcursorComments *comments = XcursorCommentsCreate (0);
+    XcursorComments *comments;
     XcursorFile	    f;
     XcursorBool	    ret;
-    if (!comments || !file || !images)
+
+    if (!file || !images)
+	return 0;
+    if ((comments = XcursorCommentsCreate (0)) == NULL)
 	return 0;
     _XcursorStdioFileInitialize (file, &f);
     ret = XcursorXcFileSave (&f, comments, images) && fflush (file) != EOF;
